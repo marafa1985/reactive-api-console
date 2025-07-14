09809f23fb30f0e78af673995dfb23cc20151458
Mahmoud Arafa <mahmoud.mohmed@check24.de>
2025-07-15 01:08:08 +0200

    RAC-11: update e2e testing link

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://reactive-api-console.vercel.app/",
+    baseUrl: "http://localhost:5173/", //"https://reactive-api-console.vercel.app/"
     supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
   },
   component: {
